ac41231a33d6a10a1a58a290720f65e39f54a1dc
yuvraj0802 <105724665+yuvraj0802@users.noreply.github.com>
2024-03-26 11:42:49 -0400

    remove vercel

--- Server/server.js
+++ Server/server.js
@@ -3,17 +3,18 @@ var cors = require('cors');
 
 const app = express(); // intiate express in app variable
 app.use(express.json()); //global middleware 
-app.use(cors({
-    origin: ['https://deploy-mern-frontend-gamma.vercel.app/'],
-    methods: ['POST','GET'],
-    credentials: true
-})) ;
+// app.use(cors({
+//     origin: ['http://localhost:3000'],
+//     methods: ['POST','GET'],
+//     credentials: true
+// })) ;
 
+app.use(cors());
 
-app.get('/', (req,res)=>{
+app.get('/', (req,res)=>{ 
     res.json('api is running')
 })
-
+ 
 const port = process.env.PORT || 4000;
 app.listen(port,function(){
     console.log(`server listening on port ${port}`); 
