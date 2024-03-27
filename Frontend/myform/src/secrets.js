e2de62c3baf265a44c48f06032c028efcd5921df
yuvraj0802 <105724665+yuvraj0802@users.noreply.github.com>
2024-03-27 13:05:00 -0400

    node_modules

--- Frontend/myform/src/secrets.js
+++ Frontend/myform/src/secrets.js
@@ -1,4 +1,3 @@
 export const BASE_URL = 'https://6603097a1392da7400a74b0f--helpful-blancmange-30b696.netlify.app'
 
 // export const BASE_URL = 'http://localhost:4000'
-// this is used for local system
